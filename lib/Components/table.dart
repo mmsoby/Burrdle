2c550553ea36b9e86c835863073e4ea3b35e9360
Mustafa Soby <mustafasoby@icloud.com>
2022-03-22 09:02:22 -0400

    userInputText field now correctly gets information to be sent to the table class.

--- lib/Components/table.dart
+++ lib/Components/table.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class DisplayTable extends StatelessWidget {
+class DisplayTable extends StatefulWidget {
   const DisplayTable({Key? key}) : super(key: key);
 
+  @override
+  State<DisplayTable> createState() => _DisplayTableState();
+}
+
+class _DisplayTableState extends State<DisplayTable> {
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/Components/userInput.dart
+++ lib/Components/userInput.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:dropdownfield2/dropdownfield2.dart';
 
-class UserInput extends StatelessWidget {
+class UserInput extends StatefulWidget {
   const UserInput({Key? key}) : super(key: key);
+
+  @override
+  State<UserInput> createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  final playersSelected = TextEditingController();
+  String selectedPlayer = "awlad soby";
+
+  List<String> players = [
+    "Josh Allen",
+    "Eli Apple",
+    "Joe Burrow",
+    "Jamar Chase"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: BoxConstraints(minWidth: 100, maxWidth: 500),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child: TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Search Players',
-          ),
-        ),
+      child: DropDownField(
+        controller: playersSelected,
+        hintText: "Select Player",
+        enabled: true,
+        items: players,
+        onValueChanged: (value) {
+          //For some reason, needed to add the extra parentheses
+          // and semicolon for this to work.
+          setState(() {
+            selectedPlayer = value;
+          });
+        },
       ),
     );
   }
